8159b21a9948d8d799181c9c93178f55193efaaf
W-eo-W <lucavester1@gmail.com>
2025-12-28 19:03:47 +0100

    .

---
+++ ConsoleMathGame.LVester/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleMathGame.LVester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
